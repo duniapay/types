ae007b7040c9dbc66b46cd6d89fd57cc0cfecbd3
Cedric Achi <cedric@duniapay.net>
2022-06-23 19:09:47 +0000

    Issues fixed

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import * as Entities from "./entities";
 import * as Enums from "./enums";
 import * as Models from "./models";
+import connection from "./connection";
 
-export { Entities, Enums, Models };
+export { Entities, Enums, Models, connection };
